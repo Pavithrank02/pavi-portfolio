7d17044df6e8693f6cbe40418e2cd5cb2cbee1ae
Pavithran2 <pavithrank02@gmail.com>
2024-04-09 18:03:52 +0530

    quotes added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Player } from '@lottiefiles/react-lottie-player';
 import { NavLink } from 'react-router-dom';
 import { motion, useAnimation } from 'framer-motion';
@@ -6,11 +6,27 @@ import { useInView } from 'react-intersection-observer';
 
 const Home = () => {
   const controls = useAnimation();
+  const [quote, setQuote] = useState("")
   const [ref, inView] = useInView({
     threshold: 0.5,
     triggerOnce: true // Optional: Remove if you want the animation to trigger every time 50% is visible
   });
+  const getSimilarMovies = async () => {
+    try {
+      const data = await fetch("https://favqs.com/api/qotd");
+      if (!data.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const json = await data.json();
+      setQuote(json.quote);
+    } catch (error) {
+      console.log('Error:', error.message);
+    }
+  };
 
+  useEffect(() => {
+    getSimilarMovies();
+  }, []);
   useEffect(() => {
     if (inView) {
       controls.start('show');
@@ -46,6 +62,11 @@ const Home = () => {
               />
             </motion.div>
           </div>
+        </div>
+        <div className='d-flex flex-column  text-center justify-content-center '>
+          <h2>"Quote of The Day" by {quote.author}</h2>
+          <h5>{quote.body}</h5>
+
         </div>
       </section>
     </motion.div>
