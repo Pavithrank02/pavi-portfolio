eb4dcec76b7e9631ed3a16f9222aabdc7c645ccf
Pavithran2 <pavithrank02@gmail.com>
2024-04-10 11:53:54 +0530

    lazy loading and shimmer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { useEffect } from 'react';
-import SharedLayout from './components/SharedLayout';
-import Home from './components/Home'
-import About from './components/About'
-import Projects from './components/CurrenProjects'
-import Contact from './components/Contact';
+import React, { useEffect, Suspense } from 'react';
 import { motion } from 'framer-motion';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MediumArticles from './components/MediumArticles';
+import { BrowserRouter } from 'react-router-dom'
+import SharedLayout from './components/SharedLayout';
 import './App.css'
-import Footer from './components/Footer';
+import Shimmer from './components/Shimmer';
+// Import `React.lazy` for other components
+const Home = React.lazy(() => import('./components/Home'));
+const About = React.lazy(() => import('./components/About'));
+const Projects = React.lazy(() => import('./components/CurrenProjects'));
+const Contact = React.lazy(() => import('./components/Contact'));
+const MediumArticles = React.lazy(() => import('./components/MediumArticles'));
+const Footer = React.lazy(() => import('./components/Footer'));
+
 const containerVariants = {
   hidden: { opacity: 0 },
   show: {
@@ -21,7 +24,6 @@ const containerVariants = {
 
 function App() {
   useEffect(() => {
-    // Automatically scroll to the "home" section on app load
     const homeSection = document.getElementById('home');
     if (homeSection) {
       window.scrollTo({
@@ -32,34 +34,34 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
-      <div>
-        <SharedLayout />
-        <motion.div initial="hidden" animate="show" variants={containerVariants}>
-          <div id="home" className="section">
-            <Home />
-          </div>
-          <div id="about" className="section">
-            <About />
-          </div>
-          <div id="project" className="section">
-            <Projects />
-          </div>
-          <div id="contact" className="section">
-            <Contact />
-          </div>
-          <div id="articles" className="section">
-            <MediumArticles />
-          </div>
-          <div >
-            <Footer />
-          </div>
-        </motion.div>
-      </div>
-    </BrowserRouter>
-  )
-
-
+    <Suspense fallback={<div><Shimmer /></div>}>
+      <BrowserRouter>
+        <div>
+          <SharedLayout />
+          <motion.div initial="hidden" animate="show" variants={containerVariants}>
+            <div id="home" className="section">
+              <Home />
+            </div>
+            <div id="about" className="section">
+              <About />
+            </div>
+            <div id="project" className="section">
+              <Projects />
+            </div>
+            <div id="contact" className="section">
+              <Contact />
+            </div>
+            <div id="articles" className="section">
+              <MediumArticles />
+            </div>
+            <div>
+              <Footer />
+            </div>
+          </motion.div>
+        </div>
+      </BrowserRouter>
+    </Suspense>
+  );
 }
 
 export default App;
